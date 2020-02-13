44e9401f0e85281e89c3c35b087e35c295367f86
Thiago Brolly <thiagoex377@gmail.com>
2020-02-13 15:08:57 -0300

    v1

---
+++ Assets/Scripts/chest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chest : MonoBehaviour {
+
+	private SpriteRenderer spriteRenderer;
+	public Sprite[] imagemObjeto;
+	public bool open;
+
+	// Use this for initialization
+	void Start () {
+		
+		spriteRenderer = GetComponent<SpriteRenderer>();
+
+	}
+	public void interacao(){
+		open = !open;
+
+		switch (open)
+		{
+			case true:
+				spriteRenderer.sprite = imagemObjeto[1];
+				break;
+
+			case false:
+				spriteRenderer.sprite = imagemObjeto[0];
+				break;
+		}
+	}
+}
